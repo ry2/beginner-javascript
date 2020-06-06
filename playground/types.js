b6ac1921fc0c8deb754f99f3ffd60bf95297b4ec
Javier Rebolledo <javier@rebolledo.mx>
2020-06-06 21:27:05 +0200

    Initialize

---
+++ playground/types.js
@@ -0,0 +1,26 @@
+// /* eslint-disable */
+// const name = 'Javier';
+// const middle = "topher";
+// const last = `bos`;
+
+// const sentence = `she's so "cool"`;
+
+// const hello = `hello my name is ${name} . Nice to meet you`
+
+// const html = `
+//     <div>
+//         <h2>${name}</h2>
+//     </div>
+// `;
+
+// document.body.innerHTML = html;
+
+// const person = {
+//   first: 'wes',
+//   last: 'bos',
+//   age: 100,
+// };
+
+const isDrawing = false;
+const age = 18;
+const ofAge = age > 19;
